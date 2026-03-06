7f63c60b5cb5f981ceba2f2d0b94143c4016fe23
Beast2live <Beast2live@github.com>
2026-03-06 18:12:20 +0100

    Configure S3 remote backend for Terraform

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,5 +1,13 @@
 terraform {
   required_version = ">= 1.5.0"
+
+  backend "s3" {
+    bucket         = "apnea-mlops-state-mahmoud123321" # Use the exact name from Step 1
+    key            = "terraform.tfstate"         # The name of the file inside the bucket
+    region         = "eu-central-1"
+    encrypt        = true                        # AES-256 encryption at rest
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
