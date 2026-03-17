805ead117aef27f6e077b7dbd0c6a5829c97b25e
Beast2live <Beast2live@github.com>
2026-03-17 23:48:41 +0100

    added correct outputs

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -10,3 +10,13 @@ output "weights_bucket_name" {
   value       = aws_s3_bucket.ml_weights.id
   description = "The name of the S3 bucket created for ML weights"
 }
+
+output "ecr_registry_url" {
+  value       = local.ecr_registry
+  description = "The URL of the AWS ECR registry"
+}
+
+output "ecr_image_url" {
+  value       = local.ecr_image
+  description = "The full URL of the ECR image including the latest tag"
+}
