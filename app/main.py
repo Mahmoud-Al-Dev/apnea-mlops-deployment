3e98206fce9c0fe490f6631b3647dce86e1bc500
Beast2live <Beast2live@github.com>
2026-03-31 14:05:21 +0200

    Fixed a bug with mounting the weights

--- app/main.py
+++ app/main.py
@@ -3,12 +3,10 @@ from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 import io
 from model import process_csv_and_predict
-
 from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(title="Apnea Detection API")
 
-# Allow Streamlit to talk to FastAPI
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -16,21 +14,17 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# --- NEW INSTRUMENTATION BLOCK ---
 Instrumentator().instrument(app).expose(app)
-# ---------------------------------
 
 @app.post("/predict_csv")
 async def predict_csv(file: UploadFile = File(...)):
-    if not file.filename.endswith('.csv'):
+    if not file.filename.endswith(".csv"):
         raise HTTPException(status_code=400, detail="Only CSV files are allowed.")
-    
+
     contents = await file.read()
-    
-    # HARDCODED: Tells pandas there are no headers, then assign them
     df = pd.read_csv(io.BytesIO(contents), header=None)
-    df.columns = ['PFlow', 'Abdomen', 'Thorax', 'SaO2', 'Vitalog1', 'Vitalog2', 'time_sec']
-    
+    df.columns = ["PFlow", "Thorax", "Abdomen", "SaO2", "Vitalog1", "Vitalog2", "time_sec"]
+
     try:
         results = process_csv_and_predict(df)
         return {"filename": file.filename, "predictions": results}
