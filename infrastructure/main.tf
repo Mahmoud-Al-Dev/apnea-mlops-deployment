96ee3419c5b3c09b61a397608b3b22b16fab5ad6
Beast2live <Beast2live@github.com>
2026-06-05 15:02:44 +0200

    Updated the model weights to the buckets

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -36,7 +36,7 @@ resource "aws_key_pair" "deployer" {
   key_name   = "${var.project_name}-key"
   public_key = file("~/.ssh/apnea_key.pub") 
 }
-
+ 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.this.id
   tags   = { Name = "${var.project_name}-igw" }
