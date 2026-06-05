afbc3518cd446302ed94a7b2bc896b670be45a58
Beast2live <Beast2live@github.com>
2026-06-05 14:35:01 +0200

    updated app und role id

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,7 +7,7 @@ data "vault_generic_secret" "aws_creds" {
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"] # Canonical
-
+ 
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
